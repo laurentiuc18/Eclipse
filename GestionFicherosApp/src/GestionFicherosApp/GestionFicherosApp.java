829141f994b552bc905c2750fad034d8fbdfa002
Lauren <37366118+laurentiuc18@users.noreply.github.com>
2020-10-15 08:24:14 +0200

    Add files via upload

---
+++ GestionFicherosApp/src/GestionFicherosApp/GestionFicherosApp.java
@@ -0,0 +1,14 @@
+package GestionFicherosApp;
+
+import gestionficheros.MainGUI;
+
+public class GestionFicherosApp {
+
+	public static void main(String[] args) {
+		
+		GestionFicherosImpl getFicherosImpl=new GestionFicherosImpl();
+		new MainGUI(getFicherosImpl).setVisible(true);
+
+	}
+
+}
